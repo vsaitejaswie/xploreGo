0695f8898f1a53abc39813c1b07e19f05ccdefec
vsaitejaswie <saitejaswie@gmail.com>
2018-11-21 19:41:04 +0530

    Declarations and Assignments

--- declarations.go
+++ declarations.go
@@ -35,13 +35,19 @@ func main(){
 	// A name is exported if it begins with a capital letter
 	// When importing a package, you can refer only to its exported names.
 	// Here Println starts with a capital letter, hence it is an exported name
+	
 	fmt.Println("This is module 1")
+
 	fmt.Println(math.Pi)
+
 	new_variable := "Hi, I'm the new member"
 	fmt.Println(new_variable)
+
 	x, y, z := multi_return(10, "Tej") // Declaration and Assignment of new variables within a function
 	fmt.Println(x, y, z)
+
 	fmt.Println(i, m, n, p, c, d, e)
+	
 	a1, a2 := try_named()
 	fmt.Println(a1, a2)
 }
